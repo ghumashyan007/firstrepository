849ec2ef2eac838009070597c197c426cd2e555d
user <user@comp14m.tumo.lab>
2018-03-24 15:39:44 +0400

    superserver

--- public/Eatgrass.js
+++ public/Eatgrass.js
@@ -1,6 +1,6 @@
-class Eatgrass {
-    constructor(x, y, ind) {
-        this.index = ind;
+class Eatgrass extends header {
+    constructor(x, y, index) {
+        universal(x, y, index);
         this.x = x;
         this.y = y;
         this.multiply = 0;
@@ -10,54 +10,20 @@ class Eatgrass {
     }
 
     newDirections() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
+        return universal.newDirections();
     }
 
 
     getDirections(t) {
-        this.newDirections();
-        var found = [];
+        
+        return universal.getDirections(t);
 
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == t) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
     }
 
 
-
-
-    move() {
-        var emptyCord = this.getDirections(0);
-        var cord = random(emptyCord);
-
-        if (cord) {
-            var x = cord[0];
-            var y = cord[1];
-
-            matrix[y][x] = 2;
-
-            matrix[this.y][this.x] = 0;
-
-            this.x = x;
-            this.y = y;
-
-        }
+    move(ch) {
+       
+       return universal.getDirections(ch)
     }
 
 
@@ -101,23 +67,9 @@ class Eatgrass {
         }
     }
 
-    mul() {
-        var emptyCord = this.getDirections(0);
-        var cord = random(emptyCord);
-        
-
-        if (cord) {
-            var x = cord[0];
-            var y = cord[1];
-
-            this.multiply++;
-
-            var norXotaker = new Eatgrass(x, y, this.index);
-            eatArr.push(norXotaker);
-
-            matrix[y][x] = 2;
-            this.multiply = 0;
-        }
+    mul(ch) {
+       
+       return universal.getDirections(ch);
     }
     die() {
         matrix[this.y][this.x] = 0;
